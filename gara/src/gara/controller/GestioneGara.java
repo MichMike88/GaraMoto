eda2fccce17b04d3ede390e49a1a86daec9125b7
Gaetano1998 <44635305+Gaetano1998@users.noreply.github.com>
2018-10-31 12:47:21 +0100

    Update GestioneGara.java

--- gara/src/gara/controller/GestioneGara.java
+++ gara/src/gara/controller/GestioneGara.java
@@ -17,7 +17,7 @@ public class GestioneGara {
 	
 	public PilotaDao cercaPilota(String nome,String cognome) throws ClassNotFoundException, SQLException {
 		
-		pdao.cercapilota(nome, cognome);
+		pdao.cercapilota(nome, cognome,codicefiscale);
 		
 		return pdao;
 	}
